8dbac8a3c1d303f291d09ed04ef5ed31ddae433c
Alain Ravet <alainravet@gmail.com>
2013-05-10 19:28:29 +0200

    move, rename, ..

--- lib/post_to_google_api_workers_pool.rb
+++ lib/post_to_google_api_workers_pool.rb
@@ -6,25 +6,27 @@ class PostToGoogleApiWorkersPool
   attr_reader :queue
 
   def initialize(nof_workers)
-    @queue  = Queue.new
-    @client = HTTPClient.new
-    spawn(nof_workers)
+    @nof_workers = nof_workers
+
+    start
   end
 
 #------------------------------------------------------------------------------
-private
+  private
 
-  def spawn(nof_workers)
-    nof_workers.times do
-      Thread.new do
-        while data = queue.pop
-          @client.post("https://android.googleapis.com/gcm/send", data, {
-              "Authorization" => "key=AIzaSyCABSTd47XeIH",
-              "Content-Type"  => "application/json"
-          })
+    def start
+      @queue  = Queue.new
+      @client = HTTPClient.new
+      @nof_workers.times do
+        Thread.new do
+          while data = queue.pop
+            @client.post("https://android.googleapis.com/gcm/send", data, {
+                "Authorization" => "key=AIzaSyCABSTd47XeIH",
+                "Content-Type"  => "application/json"
+            })
+          end
         end
       end
     end
-  end
 
 end
