9e19546f7fb986510a005d987ed446c4013b8094
Alain Ravet <alainravet@gmail.com>
2013-05-10 12:11:45 +0200

    extract + pass self as parameter to prepare the dependency inversion (?right name) introduction

--- lib/push_daemon.rb
+++ lib/push_daemon.rb
@@ -12,27 +12,30 @@ class PushDaemon
   def start
     worker   = GoogleApiWorker.new(DEFAULT_NOF_WORKERS)
     listener = Listener.new(DEFAULT_PORT)
-    process_requests(listener, worker)
+    process_requests(self, listener, worker)
+  end
+
+  def call(data, socket, worker)
+    case data[0].split.first
+      when "PING"
+        socket.send("PONG", 0, data[1][3], data[1][1])
+      when "SEND"
+        data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
+        json = JSON.generate({
+                                 "registration_ids" => [$1],
+                                 "data"             => {"alert" => $2}
+                             })
+        worker.queue << json
+    end
   end
 
 #------------------------------------------------------------------------------
 private
 
-
-  def process_requests(listener, worker)
+  def process_requests(zelf, listener, worker)
     socket = listener.socket
     while data = socket.recvfrom(maxlen=4096)
-      case data[0].split.first
-        when "PING"
-          socket.send("PONG", 0, data[1][3], data[1][1])
-        when "SEND"
-          data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
-          json = JSON.generate({
-                                   "registration_ids" => [$1],
-                                   "data" => {"alert" => $2}
-                               })
-          worker.queue << json
-      end
+      zelf.call(data, socket, worker)
     end
   end
 
