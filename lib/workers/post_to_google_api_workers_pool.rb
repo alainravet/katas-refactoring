f15216d881ecf6490560a8cbd5f32c8b186758df
Alain Ravet <alainravet@gmail.com>
2013-05-10 23:17:27 +0200

    better name : 'shared_client' <- 'client'

--- lib/workers/post_to_google_api_workers_pool.rb
+++ lib/workers/post_to_google_api_workers_pool.rb
@@ -6,9 +6,9 @@ require_relative 'post_to_google_api_worker'
 class PostToGoogleApiWorkersPool < WorkersPoolBase
 
   def create_and_start_workers(nof_workers)
-    client = HTTPClient.new
+    shared_client = HTTPClient.new
     nof_workers.times do
-      PostToGoogleApiWorker.new(@queue, client)
+      PostToGoogleApiWorker.new(@queue, shared_client)
     end
   end
 
