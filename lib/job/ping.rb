bd5500b7b23508a230ee10732c5ccb2627b504ef
Alain Ravet <alainravet@gmail.com>
2013-05-11 13:49:31 +0200

    better name : 'context' replaces 'app'

--- lib/job/ping.rb
+++ lib/job/ping.rb
@@ -4,10 +4,10 @@ module Job
 
   class Ping < Job::Base
 
-    def run(app)
+    def run(context)
       address = address_parts.addr
       port    = address_parts.port
-      app.port_binder.socket.send("PONG", 0, address, port)
+      context.port_binder.socket.send("PONG", 0, address, port)
     end
 
   end
--- lib/job/send.rb
+++ lib/job/send.rb
@@ -4,14 +4,14 @@ require 'json'
 
 class Job::Send < Job::Base
 
-  def run(app)
+  def run(context)
     api_token = command_line_tokens.rest[0]
     msg       = command_line_tokens.rest[1]
     json = JSON.generate({
                              "registration_ids" => [api_token],
                              "data"             => {"alert" => msg}
                          })
-    app.workers_pool.queue << json
+    context.workers_pool.queue << json
   end
 
 end
