9d1142a4bc0d5bf3f87bb9ccc3aa62af9bcee41f
Alain Ravet <alainravet@gmail.com>
2013-05-11 19:33:03 +0200

    extract 3 clearly separated steps into 3 super-accurately-and-longly named methods

--- lib/push_daemon.rb
+++ lib/push_daemon.rb
@@ -6,35 +6,49 @@ require "socket"
 class PushDaemon
 
   def initialize
-    client = HTTPClient.new
     queue  = Queue.new
-    10.times do
-      Thread.new do
-        while data = queue.pop
-          client.post("https://android.googleapis.com/gcm/send", data, {
-            "Authorization" => "key=AIzaSyCABSTd47XeIH",
-            "Content-Type" => "application/json"
-          })
+    start_pool_of__wait_for_internal_notification_request_and_post_to_google_notifier__workers(queue, 10)
+    socket = listening_socket_for_internal_notifications_requests("0.0.0.0", 6889)
+    wait_for_and_process_external_notifications_requests(queue, socket)
+  end
+
+  #-----------------------------------------------------------------------------
+  private
+
+    def start_pool_of__wait_for_internal_notification_request_and_post_to_google_notifier__workers(queue, pool_size)
+      client = HTTPClient.new
+      pool_size.times do
+        Thread.new do
+          while data = queue.pop
+            client.post("https://android.googleapis.com/gcm/send", data, {
+                "Authorization" => "key=AIzaSyCABSTd47XeIH",
+                "Content-Type"  => "application/json"
+            })
+          end
         end
       end
     end
 
-    socket = UDPSocket.new
-    socket.bind("0.0.0.0", 6889)
+    def listening_socket_for_internal_notifications_requests(address, port)
+      socket = UDPSocket.new
+      socket.bind(address, port)
+      socket
+    end
 
-    while data = socket.recvfrom(4096)
-      case data[0].split.first
-      when "PING"
-        socket.send("PONG", 0, data[1][3], data[1][1])
-      when "SEND"
-        data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
-        json = JSON.generate({
-          "registration_ids" => [$1],
-          "data" => { "alert" => $2 }
-        })
-        queue << json
+    def wait_for_and_process_external_notifications_requests(queue, socket)
+      while data = socket.recvfrom(4096)
+        case data[0].split.first
+          when "PING"
+            socket.send("PONG", 0, data[1][3], data[1][1])
+          when "SEND"
+            data[0][5..-1].match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
+            json = JSON.generate({
+                                     "registration_ids" => [$1],
+                                     "data"             => {"alert" => $2}
+                                 })
+            queue << json
+        end
       end
     end
-  end
 
 end
