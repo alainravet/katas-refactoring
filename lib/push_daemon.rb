9dc9e0f9eb4c5562230a2fa8f0d2f6ec2e8fc699
Alain Ravet <alainravet@gmail.com>
2013-05-12 20:51:32 +0200

    extract top-level methods with super-explicit names

--- lib/push_daemon.rb
+++ lib/push_daemon.rb
@@ -10,42 +10,52 @@ class PushDaemon
     client_to_api = HTTPClient.new
     incoming_requests_source = UDPSocket.new
 
-    10.times do
-      Thread.new do
-        while notification_post_job_payload = jobs_queue.pop
-          client_to_api.post("https://android.googleapis.com/gcm/send",
-                             notification_post_job_payload,
-          {
-            "Authorization" => "key=AIzaSyCABSTd47XeIH",
-            "Content-Type" => "application/json"
-          })
+    spawn_workers_that_post_to_api(jobs_queue, client_to_api)
+
+    incoming_requests_source.bind("0.0.0.0", 6889)
+    wait_for_and_perform_or_delegate_incoming_requests(jobs_queue, incoming_requests_source)
+  end
+
+  #-----------------------------------------------------------------------------
+  private
+
+    def spawn_workers_that_post_to_api(jobs_queue, client_to_api)
+      10.times do
+        Thread.new do
+          while notification_post_job_payload = jobs_queue.pop
+            client_to_api.post("https://android.googleapis.com/gcm/send",
+               notification_post_job_payload,
+               {
+                   "Authorization" => "key=AIzaSyCABSTd47XeIH",
+                   "Content-Type"  => "application/json"
+               })
+          end
         end
       end
     end
 
-    incoming_requests_source.bind("0.0.0.0", 6889)
+    def wait_for_and_perform_or_delegate_incoming_requests(jobs_queue, incoming_requests_source)
+      while req = incoming_requests_source.recvfrom(4096)
+        msg, sender_addrinfo = *req
+        msg_verb             = msg.split.first
+        msg_rest             = msg[5..-1]
 
-    while req = incoming_requests_source.recvfrom(4096)
-      msg, sender_addrinfo = *req
-      msg_verb = msg.split.first
-      msg_rest = msg[5..-1]
-
-      case msg_verb
-      when "PING"
-        sender_ip_address = sender_addrinfo[3]
-        sender_port       = sender_addrinfo[1]
-        incoming_requests_source.send("PONG", 0, sender_ip_address, sender_port)
-      when "SEND"
-        msg_rest.match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
-        registration_id   = $1
-        notification_text = $2
-        notification_post_job_payload = JSON.generate({
-          "registration_ids" => [registration_id],
-          "data" => { "alert" => notification_text}
-        })
-        jobs_queue << notification_post_job_payload
+        case msg_verb
+          when "PING"
+            sender_ip_address = sender_addrinfo[3]
+            sender_port       = sender_addrinfo[1]
+            incoming_requests_source.send("PONG", 0, sender_ip_address, sender_port)
+          when "SEND"
+            msg_rest.match(/([a-zA-Z0-9_\-]*) "([^"]*)/)
+            registration_id               = $1
+            notification_text             = $2
+            notification_post_job_payload = JSON.generate({
+                                                              "registration_ids" => [registration_id],
+                                                              "data"             => {"alert" => notification_text}
+                                                          })
+            jobs_queue << notification_post_job_payload
+        end
       end
     end
-  end
 
 end
